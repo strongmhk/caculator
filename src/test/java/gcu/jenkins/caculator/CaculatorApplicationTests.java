dad920f1021d25220554668d999f4a7221be8e58
strongmhk <kim6562166086@gmail.com>
2024-11-12 10:39:07 +0900

    initial commit

---
+++ src/test/java/gcu/jenkins/caculator/CaculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package gcu.jenkins.caculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
